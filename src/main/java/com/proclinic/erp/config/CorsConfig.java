6513e722b88d6362838288528c41496b4261e661
AZIZI <65093603+MedAZIZI@users.noreply.github.com>
2026-06-11 22:33:07 +0200

    test Cors permission 1!

--- src/main/java/com/proclinic/erp/config/CorsConfig.java
+++ src/main/java/com/proclinic/erp/config/CorsConfig.java
@@ -1,6 +1,9 @@
 package com.proclinic.erp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,5 +24,16 @@ public class CorsConfig implements WebMvcConfigurer {
                     .allowedOrigins("*")
                     .allowedMethods("*")
                     .allowedHeaders("*");
+        }
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http
+                    .cors(cors -> {})
+                    .csrf(csrf -> csrf.disable())
+                    .authorizeHttpRequests(auth -> auth
+                            .anyRequest().permitAll()
+                    );
+
+            return http.build();
         }
 }
